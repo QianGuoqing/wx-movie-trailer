53613d206b04b4a78289341d532d691b06c5f5f6
QianGuoqing <qian_guoqing@yeah.net>
2018-05-07 22:56:06 +0800

    使用template组件化movie-item，完成首页电影列表显示，实现上拉加载更多数据以及加载过程中的loading效果

--- movie-trailer/pages/index/index.js
+++ movie-trailer/pages/index/index.js
@@ -1,7 +1,39 @@
+import { requestMovieList } from '../../common/js/request'
 const app = getApp()
 
 Page({
   data: {
-   
+    page: 1,
+    size: 6,
+    movies: [],
+    loading: true
+  },
+  onLoad() {
+    this.getMovieList()
+  },
+  getMovieList() {
+    this.setData({ loading: true })
+    const { page, size } = this.data
+    requestMovieList(page, size).then(res => {
+      res = res.data
+      const data = res.data
+      let movies = [...this.data.movies]
+      data.forEach(item => {
+        movies.push(item)
+      })
+      this.setData({
+        movies,
+        loading: false
+      })
+      // console.log(this.data.movies)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  getMoreMovies() {
+    let page = this.data.page
+    page = page + 1
+    this.setData({ page })
+    this.getMovieList()
   }
 })
