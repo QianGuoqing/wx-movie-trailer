ca73bdd952dade1528ad63062cb281059a6ca696
QianGuoqing <qian_guoqing@yeah.net>
2018-05-08 22:03:26 +0800

    创建category-item组件，完成电影分类页面

--- movie-trailer/pages/category/category.js
+++ movie-trailer/pages/category/category.js
@@ -1,1 +1,26 @@
-Page({})
+Page({
+  data: {
+    types: [
+      {
+        name: '喜剧',
+        img: '/assets/image/xiju.png'  
+      },
+      {
+        name: '动作',
+        img: '/assets/image/dongzuo.png'
+      },
+      {
+        name: '科幻',
+        img: '/assets/image/kehuan.png'
+      },
+      {
+        name: '爱情',
+        img: '/assets/image/aiqing.png'
+      },
+      {
+        name: '动画',
+        img: '/assets/image/donghua.png'
+      }
+    ]
+  }
+})
